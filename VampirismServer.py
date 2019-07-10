800dff2cb858bc8abe767cc5f89b7a51c61215d8
Christopher Klemenz <26851310+KlemChri@users.noreply.github.com>
2019-07-10 11:11:14 +0200

    Update VampirismServer.py

--- VampirismServer.py
+++ VampirismServer.py
@@ -18,7 +18,7 @@ client.remove_command("help")
 @client.event
 async def on_ready():
     while True:
-        await client.change_presence(activity=discord.Game(name='klemchri.de - offline'))
+        await client.change_presence(activity=discord.Game(name='klemchri.eu'))
         await asyncio.sleep(10)
         await client.change_presence(activity=discord.Game(name='vampirism.maxanier.de'))
         await asyncio.sleep(10)
@@ -291,34 +291,3 @@ async def info_error(ctx, error):
 
 
 client.run(TOKEN)
-
-# HTTPRequestHandler class
-class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
-
-  # GET
-  def do_GET(self):
-        # Send response status code
-        self.send_response(200)
-
-        # Send headers
-        self.send_header('Content-type','text/html')
-        self.end_headers()
-
-        # Send message back to client
-        message = "Hello world!"
-        # Write content as utf-8 data
-        self.wfile.write(bytes(message, "utf8"))
-        return
-
-def run():
-  print('starting server...')
-
-  # Server settings
-  # Choose port 8080, for port 80, which is normally used for a http server, you need root access
-  server_address = ('127.0.0.1', 8081)
-  httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
-  print('running server...')
-  httpd.serve_forever()
-
-
-run()
