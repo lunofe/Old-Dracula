bdd2cae0c3961b7c4e2a9c3d00351c36c4728481
1LiterZinalco <1literzinalco@chimute.org>
2020-01-31 19:02:22 +0100

    Try Logging

--- cogs/MessageHandler.py
+++ cogs/MessageHandler.py
@@ -7,7 +7,7 @@
 #    o888bood8P'   d888b    `Y888""8o `Y8bod8P'  `V88V"V8P' o888o `Y888""8o o888bood8P'  `Y8bod8P'   "888"
 #:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-import os, discord, asyncio, datetime
+import os, discord, asyncio, datetime, logging
 from discord.ext import commands
 from discord import User
 import time, os, asyncio
